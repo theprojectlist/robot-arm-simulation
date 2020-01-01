9cef34e504ffa88947d67b1361acf9508b57f328
Colin Snow <colinmsnow@gmail.com>
2020-01-01 11:59:49 -0500

    Add camera to end of arm which follows movements

--- PyBullet_Experiments/robot_arm_position_control.py
+++ PyBullet_Experiments/robot_arm_position_control.py
@@ -2,6 +2,8 @@
 import pybullet as p
 import time
 import pybullet_data
+from pyquaternion import Quaternion
+
 p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath()) #optionally
 
@@ -34,6 +36,38 @@ while (True):
     positions = p.calculateInverseKinematics(kuka, 6, [target_x,target_y,target_z])
     p.setJointMotorControlArray(kuka, [0,1,2,3,4,5,6], p.POSITION_CONTROL,
         targetPositions = positions,
-        forces = [maxForce, maxForce,maxForce, maxForce,maxForce, maxForce, maxForce])
+        forces = [maxForce, maxForce, maxForce, maxForce, maxForce, maxForce, maxForce])
+
+
+    ### Camera Rendering ###
+
+    position, orientation,_,_,_,_ = p.getLinkState(kuka, 6)
+
+    print(orientation)
+
+    initialVector = [0,0,1]
+    rotation = Quaternion(orientation[3],orientation[0],orientation[1],orientation[2])
+    rotatedVector = rotation.rotate(initialVector)
+
+
+    viewMatrix = p.computeViewMatrix(
+    cameraEyePosition=position,
+    cameraTargetPosition=[position[0]+rotatedVector[0],position[1]+rotatedVector[1],position[2]+rotatedVector[2]],
+    cameraUpVector=[0, 0, 1])
+
+    projectionMatrix = p.computeProjectionMatrixFOV(
+            fov=45.0,
+            aspect=1.0,
+            nearVal=0.1,
+            farVal=3.1)
+
+    width, height, rgbImg, depthImg, segImg = p.getCameraImage(
+            width=224, 
+            height=224,
+            viewMatrix=viewMatrix,
+            projectionMatrix=projectionMatrix)
+
+    p.addUserDebugLine([position[0],position[1],position[2]],[position[0]+rotatedVector[0],position[1]+rotatedVector[1],position[2]+rotatedVector[2]], lifeTime=1, lineColorRGB=[0,1,0])
+    # p.addUserDebugLine([0,0,0], rotatedVector, lifeTime=1, lineColorRGB=[1,0,0])
 
     time.sleep(1./240.)
